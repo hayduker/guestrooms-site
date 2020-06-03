d930d5f5db939d7f235e2d7da0a73bfef637ba61
Derek Smith <dcsmith1212@gmail.com>
2020-06-03 13:21:12 -0600

    Replace music player with just play/pause button, add audio

--- script.js
+++ script.js
@@ -1,66 +1,38 @@
-const title = document.querySelector('.song-title');
 const playBtn = document.getElementById('play');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
 const musicContainer = document.querySelector('.music-player');
+const audio = document.getElementById('audio');
 
-// Song titles
-const songs = ['etc.', 'Put Me Together', 'Asleep at the Wheel', 'Duval'];
-
-// Keep track of song
-let songIndex = 1;
-
-// Initially load song details into DOM
-loadSong(songs[songIndex]);
-
-// Update song details
-function loadSong(song) {
-    title.innerText = song;
-}
+let playing = false;
 
 // Play song
 function playSong() {
-    musicContainer.classList.add('play');
+    playing = true;
     playBtn.querySelector('i.fas').classList.remove('fa-play');
     playBtn.querySelector('i.fas').classList.add('fa-pause');
+    audio.play();
 }
 
 // Pause song
 function pauseSong() {
-    musicContainer.classList.remove('play');
+    playing = false;
     playBtn.querySelector('i.fas').classList.remove('fa-pause');
     playBtn.querySelector('i.fas').classList.add('fa-play');
+    audio.pause();
 }
 
-// Previous song
-function prevSong() {
-    songIndex--;
-    if (songIndex < 0) songIndex = songs.length - 1;
-    loadSong(songs[songIndex]);
-    playSong();
-}
-
-// Next song
-function nextSong() {
-    songIndex++;
-    if (songIndex > songs.length - 1) songIndex = 0;
-    loadSong(songs[songIndex]);
-    playSong();
-}
 
 // Event listeners
 playBtn.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play');
-
-    if (isPlaying) {
+    if (playing) {
         pauseSong();
     } else {
         playSong();
     }
 });
 
-// Change song
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
-
-console.log('yo');
+audio.addEventListener('ended', () => {
+    console.log('song ended');
+    playing = false;
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+});
