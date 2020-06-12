08d57e029f78bd4efa7fe80cc07b7f0873ca22b8
Derek Smith <dcsmith1212@gmail.com>
2020-06-12 15:53:43 -0600

    Rework project into webpack

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const playBtn = document.getElementById('play');
+const musicContainer = document.querySelector('.music-player');
+const audio = document.getElementById('audio');
+
+let playing = false;
+
+// Play song
+function playSong() {
+    playing = true;
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+    audio.play();
+}
+
+// Pause song
+function pauseSong() {
+    playing = false;
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    audio.pause();
+}
+
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+    if (playing) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+
+audio.addEventListener('ended', () => {
+    playing = false;
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+});
